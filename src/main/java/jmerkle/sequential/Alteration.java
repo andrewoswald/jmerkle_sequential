eaf2cdb2f187d1884eb4b77945c3e9d1177432e8
Andrew Oswald <andrew.oswald@gmail.com>
2011-05-03 15:07:42 -0400

    additional documentation.

--- src/main/java/jmerkle/sequential/Alteration.java
+++ src/main/java/jmerkle/sequential/Alteration.java
@@ -18,13 +18,14 @@ package jmerkle.sequential;
 import java.io.Serializable;
 
 /**
- * Encapsulates an identifiable value that is used to alter the
- * contents of a JMerkle structure.  If the intent of the alteration
- * is to <i>remove</i> a leaf from the tree, the Alteration's value
- * should be null.  All alter operations are idempotent, therefore 
- * deleting a value that does not exist has no effect and similarly,
- * adding or updating an existing value to its current value also
- * has no outward effect.
+ * Encapsulates an identifiable leaf value that is used to alter the
+ * contents of a <code>JMerkle</code> structure.  If the intent
+ * of the alteration is to <i>remove</i> a leaf from the tree, the 
+ * <code>Alteration</code>'s value should be null (the leaf identified
+ * by the key will be removed).  All alter operations are idempotent,
+ * therefore deleting a value that does not exist has no effect and 
+ * similarly, adding or updating an existing value to its current value 
+ * also has no outward effect.
  * 
  * @author andrew oswald
  *
