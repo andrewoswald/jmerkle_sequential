0612771af401e76c98ec21f3a86d56625a52256e
Andrew Oswald <andrew.oswald@gmail.com>
2011-05-03 13:56:52 -0400

    fleshing out impl.

--- src/main/java/jmerkle/sequential/Branch.java
+++ src/main/java/jmerkle/sequential/Branch.java
@@ -15,6 +15,130 @@
  */
 package jmerkle.sequential;
 
-public class Branch extends JMerkle {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+public class Branch extends JMerkle {
+	
+	private static final long serialVersionUID = 240333586693350332L;
+	
+	transient int offset;
+	
+	HashMap<Byte, JMerkle> children = new HashMap<Byte, JMerkle>();
+	
+	/*default*/ Branch() {}
+	
+	@Override
+	JMerkle alterInternal(int offset, List<Alteration> alterations) {
+		
+		if(alterations != null) {
+			
+			int alterationsSize = alterations.size();
+			
+			Map<Byte, List<Alteration>> collisions = new HashMap<Byte, List<Alteration>>();
+			
+			for(int i=0; i<alterationsSize; i++) {
+				Alteration alteration = alterations.get(i);
+				
+				byte[] keyBytes = alteration.key.getBytes();
+				byte offsetKey = JMerkle.hash(keyBytes)[offset];
+				
+				if(children.containsKey(offsetKey)) {
+					List<Alteration> collisionAlterations = collisions.get(offsetKey);
+					if(collisionAlterations == null) {
+						collisionAlterations = new ArrayList<Alteration>();
+						collisions.put(offsetKey, collisionAlterations);
+					}
+					collisionAlterations.add(alteration);
+				} else if(alteration.value != null) {
+					//we're in accordance w/ our balance rules...
+					//create and insert the new Leaf:
+					Leaf leaf = new Leaf(keyBytes, JMerkle.hash(alteration.value));
+					children.put(offsetKey, leaf);
+				}
+			}
+			
+			Set<Byte> collisionKeys = collisions.keySet();
+			for(Byte collisionKey : collisionKeys) {
+				JMerkle child = children.get(collisionKey);
+				List<Alteration> pendingAlterations = collisions.get(collisionKey);
+				if(child.isBranch()) {
+					child.alterInternal(offset + 1, pendingAlterations);
+				} else {
+					//the alteration insert result on a leaf can result in 1) an update to that leaf,
+					//2) deletion of that leaf, or 3) conversion of that leaf into a branch:
+					JMerkle result = child.alterInternal(offset + 1, pendingAlterations);
+					if(result != null) {
+						//we don't care whether the leaf was just updated or converted:
+						children.put(collisionKey, result);
+					} else {
+						//if null, the leaf alteration was a 'delete' (isUpate == false):
+						children.remove(collisionKey);
+					}
+				}
+			}
+		}
+		
+		switch (children.size()) {
+		case 0:
+			return null;
+		default:
+			int childBytes = 0;
+			for(JMerkle jMerkle : children.values()) {
+				childBytes += jMerkle.offset();
+			}
+			this.offset = 27 + children.size() + childBytes;
+			this.hashVal = null;
+			this.hashVal = JMerkle.hash(this);
+			return this;
+		}
+	}
+	
+	@Override
+	List<UserKeyWrapper> allKeysInternal() {
+		List<UserKeyWrapper> allkeys = new ArrayList<UserKeyWrapper>();
+		
+		Collection<JMerkle> childValues = children.values();
+		for(JMerkle jMerkle : childValues) {
+			allkeys.addAll(jMerkle.allKeysInternal());
+		}
+		
+		return allkeys;
+	}
+	
+	@Override
+	boolean isBranch() {
+		return true;
+	}
+	
+	/*default*/ Boolean contains(Leaf leaf) {
+		//cycle through all the children:
+		Collection<JMerkle> childValues = children.values();
+		for(JMerkle jMerkle : childValues) {
+			if(jMerkle.isBranch()) {
+				//if branch, rinse and repeat:
+				Boolean contains = ((Branch)jMerkle).contains(leaf);
+				if(contains != null) return contains;
+			} else {
+				Leaf childLeaf = (Leaf) jMerkle;
+				//if leaf, check its userKey:
+				if(Arrays.equals(childLeaf.userKey, leaf.userKey)) {
+					//if userKeys are equal, enable 
+					//upstream short-circuit on hashVal:
+					return Arrays.equals(childLeaf.hashVal, leaf.hashVal);
+				}
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	int offset() {
+		return offset;
+	}
 }
--- src/main/java/jmerkle/sequential/JMerkle.java
+++ src/main/java/jmerkle/sequential/JMerkle.java
@@ -15,6 +15,230 @@
  */
 package jmerkle.sequential;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public abstract class JMerkle {
 
+	private static final long serialVersionUID = 7487888709693360107L;
+	
+	/*default*/ byte[] hashVal;
+	
+	/*default*/ static class UserKeyWrapper {
+		public byte[] bytes;
+		
+		UserKeyWrapper(byte[] bytes) {
+			this.bytes = bytes;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			return Arrays.equals(bytes, ((UserKeyWrapper)obj).bytes);
+		}
+	}
+	
+	public List<String> diff(JMerkle jMerkle) {
+		List<UserKeyWrapper> internalDiff = diffInternal(jMerkle);
+		
+		int internalDiffSize = internalDiff.size();
+		
+		List<String> diff = new ArrayList<String>(internalDiffSize);
+		
+		for(UserKeyWrapper ukw : internalDiff) {
+			diff.add(new String(ukw.bytes));
+		}
+		
+		return diff;
+	}
+	
+	public JMerkle alter(List<Alteration> alterations) {
+		
+		JMerkle jMerkle = alterInternal(0, alterations);
+		
+		return jMerkle;
+	}
+	
+	public List<String> allKeys() {
+		List<UserKeyWrapper> allKeysInternal = allKeysInternal();
+		
+		int allKeysInternalSize = allKeysInternal.size();
+		
+		List<String> allKeys = new ArrayList<String>(allKeysInternalSize);
+		
+		for(UserKeyWrapper ukw : allKeysInternal) {
+			allKeys.add(new String(ukw.bytes));
+		}
+		
+		return allKeys;
+	}
+	
+	/*default*/ abstract boolean isBranch();
+	
+	/*default*/ abstract JMerkle alterInternal(int offset, List<Alteration> alterations);
+	
+	/*default*/ abstract List<UserKeyWrapper> allKeysInternal();
+	
+	/*default*/ abstract int offset();
+	
+	/**
+	 * This method returns a byte[] of size 20.
+	 * <br/>
+	 * <b>Sha-1 is used to create the byte[]; its
+	 * usage is <i>not</i> for cryptographic purposes.</b>
+	 * @param obj
+	 * @return
+	 */
+	/*default*/ static final byte[] hash(Object obj) {
+		byte[] bytes = JMerkle.getBytes(obj);
+		MessageDigest digest = null;
+		try {
+			digest = MessageDigest.getInstance("SHA-1");
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+		return digest.digest(bytes);
+	}
+	
+	private static byte[] getBytes(Object obj) {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream(); 
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(bos); 
+			oos.writeObject(obj);
+			oos.flush(); 
+			oos.close(); 
+			bos.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	      byte [] data = bos.toByteArray();
+	      return data;
+	}
+	
+	private List<UserKeyWrapper> diffInternal(JMerkle that) {
+		boolean thatIsBranch = that.isBranch();
+		if(this.isBranch()) {
+			if(thatIsBranch) {
+				return diff((Branch)this, (Branch)that);
+			} else {
+				return diff((Leaf)that, (Branch)this);
+			}
+		} else {
+			if(thatIsBranch) {
+				return diff((Leaf)this, (Branch)that);
+			} else {
+				return diff((Leaf)this, (Leaf)that);
+			}
+		}
+	}
+	
+	private List<UserKeyWrapper> diff(Leaf thisLeaf, Leaf thatLeaf) {
+		
+		byte[] thisLeafUserKey = thisLeaf.userKey;
+		byte[] thatLeafUserKey= thatLeaf.userKey;
+		
+		if(thisLeafUserKey == null) {
+			if(thatLeafUserKey != null) {
+				return Collections.singletonList(new UserKeyWrapper(thatLeafUserKey));
+			} else {
+				return Collections.emptyList();
+			}
+		} else {
+			if(thatLeafUserKey == null) {
+				return Collections.singletonList(new UserKeyWrapper(thisLeafUserKey));
+			} else {
+				
+				List<UserKeyWrapper> keys = null;
+				
+				if(Arrays.equals(thisLeafUserKey, thatLeafUserKey)) {
+					if(Arrays.equals(thisLeaf.hashVal, thatLeaf.hashVal)) {
+						keys = Collections.emptyList();
+					} else {
+						keys = Collections.singletonList(new UserKeyWrapper(thisLeafUserKey));
+					}
+				} else {
+					//if they're different, return both:
+					keys = new ArrayList<UserKeyWrapper>(2);
+					keys.add(new UserKeyWrapper(thatLeafUserKey));
+					keys.add(new UserKeyWrapper(thatLeafUserKey));
+				}
+				
+				return keys;
+			}
+		}
+	}
+	
+	private List<UserKeyWrapper> diff(Leaf leaf, Branch branch) {
+		
+		List<UserKeyWrapper> diffKeys = branch.allKeysInternal();
+		
+		Boolean contains = branch.contains(leaf);
+		
+		UserKeyWrapper leafUserKeyWrapper = new UserKeyWrapper(leaf.userKey);
+		
+		if(contains != null && contains) {
+			diffKeys.remove(leafUserKeyWrapper);
+		} else {
+			//it might already be there, but we need to be sure:
+			//(to clarify, it might already be there by the virtue
+			// that the branch's leaf and the provided leaf's hash
+			// vals are indeed different.)
+			diffKeys.add(leafUserKeyWrapper);
+		}
+		
+		return diffKeys;
+	}
+	
+	private List<UserKeyWrapper> diff(Branch b1, Branch b2) {
+		if(Arrays.equals(b1.hashVal, b2.hashVal)) {
+			return Collections.emptyList();
+		} else {
+			
+			List<UserKeyWrapper> diffKeys = new ArrayList<UserKeyWrapper>();
+			
+			Map<Byte, JMerkle> b1Children = b1.children;
+			Map<Byte, JMerkle> b2Children = b2.children;
+			
+			
+			//recursive diff on keys in common:
+			Set<Byte> commonKeys = new HashSet<Byte>(b1Children.keySet());
+			commonKeys.retainAll(new HashSet<Byte>(b2Children.keySet()));
+			
+			for(Byte commonKey : commonKeys) {
+				JMerkle b1Child = b1Children.get(commonKey);
+				JMerkle b2Child = b2Children.get(commonKey);
+				diffKeys.addAll(b1Child.diffInternal(b2Child));
+			}
+			
+			//all user keys on keys unique to b1:
+			Set<Byte> b1UniqueKeys = new HashSet<Byte>(b1Children.keySet());
+			b1UniqueKeys.removeAll(new HashSet<Byte>(b2Children.keySet()));
+			
+			for(Byte b1UniqueKey : b1UniqueKeys) {
+				JMerkle b1Child = b1Children.get(b1UniqueKey);
+				diffKeys.addAll(b1Child.allKeysInternal());
+			}
+			
+			//all user keys on keys unique to b2:
+			Set<Byte> b2UniqueKeys = new HashSet<Byte>(b2Children.keySet());
+			b2UniqueKeys.removeAll(new HashSet<Byte>(b1Children.keySet()));
+			
+			for(Byte b2UniqueKey : b2UniqueKeys) {
+				JMerkle b2Child = b2Children.get(b2UniqueKey);
+				diffKeys.addAll(b2Child.allKeysInternal());
+			}
+			
+			return diffKeys;
+		}
+	}
 }
