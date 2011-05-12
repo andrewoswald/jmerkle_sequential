837ff20b8852992fe7032aae9a5d9d0a58311d65
Andrew Oswald <andrew.oswald@gmail.com>
2011-05-11 22:38:28 -0400

    added license.

--- src/main/java/jmerkle/sequential/JMerkleAlterable.java
+++ src/main/java/jmerkle/sequential/JMerkleAlterable.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011, Andrew Oswald
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package jmerkle.sequential;
 
 import java.io.Serializable;
@@ -9,10 +24,7 @@ import java.io.Serializable;
  * should be null (the leaf identified by the key will be removed). <br/>
  * Alter operations are generally idempotent. For example, deleting a value that
  * does not exist has no effect and similarly, adding or updating an existing
- * value to its current value also has no outward effect.
- * 
- * @author andrew oswald
- * 
+ * value to its current value also has no outward effect. * 
  */
 public interface JMerkleAlterable {
 
